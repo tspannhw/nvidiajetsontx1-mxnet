f329d87254aed01279dbe8aed477b014982c5ad8
Timothy Spann <tspann@hortonworks.com>
2017-10-20 16:10:43 -0400

    Create runclassify.sh

---
+++ runclassify.sh
@@ -0,0 +1,2 @@
+cd /media/nvidia/96ed93f9-7c40-4999-85ba-3eb24262d0a5/jetson-inference-master/build/aarch64/bin/
+/media/nvidia/96ed93f9-7c40-4999-85ba-3eb24262d0a5/jetson-inference-master/build/aarch64/bin/imagenet-console /media/nvidia/96ed93f9-7c40-4999-85ba-3eb24262d0a5/images/$1 /media/nvidia/96ed93f9-7c40-4999-85ba-3eb24262d0a5/images/cfout-$1
